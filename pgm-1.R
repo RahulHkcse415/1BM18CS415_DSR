738da28ed043353e7d2a09f0fbc5ab16e0d0a103
RahulHkcse415 <53852082+RahulHkcse415@users.noreply.github.com>
2020-09-29 12:38:32 +0530

    Add files via upload

---
+++ pgm-1.R
@@ -0,0 +1,5 @@
+n1<-c(10,20,30,40)
+st1<-c("water" ,"air" ,"wind")
+Rlnum<-c(23.5,55.2,234.3)
+list1<-list(n1,st1,Rlnum)
+list1
---
+++ pgm-2.R
@@ -0,0 +1,11 @@
+bufftail<-c(10,1,37,5,12)
+gardenbee<-c(8,3,9,6,4)
+redtail<-c(18,9,12,4,6)
+carderbee<-c(8,27,6,32,23)
+honeybee<-c(12,13,16,9,10)
+beeframe<-data.frame(bufftail,gardenbee,redtail,carderbee,honeybee)
+beeframe
+names<-c("THRISTEL","VIPERS","GOLDENRIVERS","YELLOWALFALA","BLACKBERRY")
+rownames(beeframe)<-names
+beeframe
+
